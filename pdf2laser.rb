69ce0ae09ea9c99c2ef450da878b5fd3ebb72594
Zachary Elliott <zach@nyu.edu>
2015-01-12 23:40:45 -0500

    pdf2laser 0.2.0-1

--- pdf2laser.rb
+++ pdf2laser.rb
@@ -1,7 +1,9 @@
 class Pdf2laser < Formula
-  url "http://zell.io/dist/pdf2laser/pdf2laser-0.2.0.tar.gz"
+  url "http://zell.io/dist/src/pdf2laser/pdf2laser-0.2.0.tar.gz"
   homepage "https://github.com/zellio/pdf2laser"
   sha1 "e61a97327206698f29519f1fa9173cfe6d7a17d5"
+
+  revision 1
 
   depends_on "ghostscript"
 
