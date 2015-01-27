24f3cfe0af3b02dcfde912c0edbfd409c9ae8df7
Zachary Elliott <zach@nyu.edu>
2015-01-27 12:18:02 -0500

    pdf2laser 0.3.3

--- pdf2laser.rb
+++ pdf2laser.rb
@@ -1,12 +1,12 @@
 class Pdf2laser < Formula
-  url "http://zell.io/dist/src/pdf2laser/pdf2laser-0.3.1.tar.gz"
+  url "http://zell.io/dist/src/pdf2laser/pdf2laser-0.3.3.tar.gz"
   homepage "https://github.com/zellio/pdf2laser"
-  sha1 "b190443b7c834b3afdcb671e7e3ec93be498c12c"
+  sha1 "b47d6faa90aa112b4fd52d4d59c96a929541d208"
 
   depends_on "ghostscript"
 
   def install
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}", "--sysconfdir=#{etc}"
     system "make"
     system "make", "install"
   end
