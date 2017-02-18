4a1ba42e24d9d34b1d64b0e33e8e08675f42558b
Zachary Elliott <contact@zell.io>
2017-02-18 15:04:31 -0500

    Update to sha256

--- pdf2laser.rb
+++ pdf2laser.rb
@@ -1,7 +1,7 @@
 class Pdf2laser < Formula
   url "http://zell.io/dist/src/pdf2laser/pdf2laser-0.3.3.tar.gz"
   homepage "https://github.com/zellio/pdf2laser"
-  sha1 "b47d6faa90aa112b4fd52d4d59c96a929541d208"
+  sha256 "e3258570ce01aff54f6870aaeec606f56c20e7683b1f6a1b0b9452b80722f600"
 
   depends_on "ghostscript"
 
