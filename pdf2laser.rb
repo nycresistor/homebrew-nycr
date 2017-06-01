89c1093cabfe58c93e41a79223d858c40545c134
Zachary Elliott <zach@noom.com>
2017-06-01 17:38:30 -0400

    pdf2laser 0.4.0

--- pdf2laser.rb
+++ pdf2laser.rb
@@ -1,12 +1,19 @@
 class Pdf2laser < Formula
-  url "http://zell.io/dist/src/pdf2laser/pdf2laser-0.3.3.tar.gz"
+  url "https://github.com/zellio/pdf2laser/archive/v0.4.0.tar.gz"
   homepage "https://github.com/zellio/pdf2laser"
-  sha256 "e3258570ce01aff54f6870aaeec606f56c20e7683b1f6a1b0b9452b80722f600"
+  sha256 "f04524b882dde1ffe26c1f967169217c3c7607a9c7b93a2d2a3a8d50c5719e92"
 
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "ghostscript"
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--sysconfdir=#{etc}"
+    system "aclocal", "-I", "m5", "--install"
+    system "autoheader"
+    system "glibtoolizei", "--force"
+    system "automake", "--add-missing", "--foreign"
+    system "autoconf"
+    system "./configure"
     system "make"
     system "make", "install"
   end
