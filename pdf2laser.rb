c5696f0584e08d90d583bb332c188bed2f3209f2
Zachary Elliott <zach@noom.com>
2017-06-01 18:28:35 -0400

    Actually release version 0.4.0

--- pdf2laser.rb
+++ pdf2laser.rb
@@ -1,6 +1,7 @@
 class Pdf2laser < Formula
-  url "https://github.com/zellio/pdf2laser/archive/v0.4.0.tar.gz"
+  desc "CLI tool printing pdf files to an Epilog laser cutter over the network"
   homepage "https://github.com/zellio/pdf2laser"
+  url "https://github.com/zellio/pdf2laser/archive/v0.4.0.tar.gz"
   sha256 "f04524b882dde1ffe26c1f967169217c3c7607a9c7b93a2d2a3a8d50c5719e92"
 
   depends_on "automake" => :build
@@ -14,8 +15,11 @@ class Pdf2laser < Formula
     system "glibtoolize", "--force"
     system "automake", "--add-missing", "--foreign"
     system "autoconf"
-    system "./configure"
+    system "./configure", "--prefix=#{prefix}"
     system "make"
     system "make", "install"
+  end
+
+  test do
   end
 end
