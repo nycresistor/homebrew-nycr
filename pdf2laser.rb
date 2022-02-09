1c82982c46695d9b4918e852d12c1fac071a3ff4
Zachary Elliott <contact@zell.io>
2022-02-09 13:52:53 -0500

    Bumps version of pdf2laser to 1.0.1

--- pdf2laser.rb
+++ pdf2laser.rb
@@ -1,8 +1,8 @@
 class Pdf2laser < Formula
   desc "CLI tool printing pdf files to an Epilog laser cutter over the network"
   homepage "https://github.com/zellio/pdf2laser"
-  url "https://github.com/zellio/pdf2laser/archive/v1.0.0.tar.gz"
-  sha256 "01028395ccc8a12f95805c9d107e3f606a3a7d4bcbc69399563d1e1d47099af0"
+  url "https://github.com/zellio/pdf2laser/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "4c4b27c7e6f3eba9d5b9785e15e8bb88f871d6da79ca513af4cf04bb69980c89"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -13,9 +13,6 @@ class Pdf2laser < Formula
 
   def install
     ENV["PATH"] += ":/usr/local/bin:/opt/homebrew/bin"
-    inreplace "configure.ac" do |conf|
-      conf.gsub!(/AC_PROG_LEX.*/, "AC_PROG_LEX")
-    end
     system "aclocal", "-I", "m4", "--install"
     system "autoheader"
     system "glibtoolize", "--force"
